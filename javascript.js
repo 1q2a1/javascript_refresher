4a9600485e2676b3273578c77bdd603f93f4170d
Mitchell <72991363+1q2a1@users.noreply.github.com>
2023-08-14 17:10:05 +0800

    Added game logic for multiple rounds
    logic for running playRound multiple times to play serveral rounds of the game and tally final results

--- javascript.js
+++ javascript.js
@@ -26,8 +26,9 @@ let playRound = (computerSelection, playerSelection) => {
     posInArrayCom = SELECTIONS.indexOf(computerSelection)
     posInArrayPlayer = SELECTIONS.indexOf(playerSelection)
 
-    console.log(`Computer selected ${computerSelection}`)
     console.log(`You selected ${playerSelection}`)
+    console.log(`Computer selected ${computerSelection}`)
+    
 
     if (posInArrayCom === posInArrayPlayer){
         console.log("DRAW")
@@ -43,6 +44,33 @@ let playRound = (computerSelection, playerSelection) => {
     }
 }
 
+let game = () => {
+    console.log("This is rock paper scissors, 5 rounds will be played")
+    let computerScore = 0
+    let playerScore = 0
+    for (let i=0; i<5; i++){
+        console.log(`Round ${i}`)
+        console.log(`Your score: Computer score is ${playerScore}:${computerScore}`)
+        let roundResult = playRound(getComputerSelection(), getPlayerSelection())
+        if (roundResult === "win"){
+            playerScore++ 
+        }
+        else if (roundResult === "lose"){
+            computerScore++
+        }
+    }
+    
+    console.log(`Final Score is ${playerScore}:${computerScore}`)
+    if (playerScore>computerScore){
+        console.log("You Win!")
+    }
+    else if(playerScore<computerScore){
+        console.log("You Lose!")
+    }
+    else{
+        console.log("Draw!")
+    }
+}
 
+game()
 
-playRound(getComputerSelection(), getPlayerSelection())
