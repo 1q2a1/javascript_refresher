0fc4ce54354521875751776ceccf478f92425a0b
Mitchell <72991363+1q2a1@users.noreply.github.com>
2023-08-17 16:34:05 +0800

    Added score counting
    scores are tracked and displayed on the ui

--- javascript.js
+++ javascript.js
@@ -1,34 +1,23 @@
 const SELECTIONS = ["rock", "paper", "scissors"]
 
+let computerScore = 0
+let playerScore = 0
+let playerScoreDisp = document.querySelector(".player-score")
+let computerScoreDisp = document.querySelector(".computer-score")
+
 let getComputerSelection = () => {
     let randNum = Math.floor(Math.random()*3)
     let selection = SELECTIONS[randNum]
     return selection
 }
 
-let getPlayerSelection = () => {
-    let playerInput
-    while (true){
-        playerInput = prompt("what would u like to select?")
-        if (!SELECTIONS.includes(playerInput.toLowerCase())){
-            console.log("Invalid Selection")
-        }
-        else{
-            break
-        }
-    }
-    return playerInput.toLowerCase()
-}
 
 let playRound = (computerSelection, playerSelection) => {
-    let posInArrayCom
-    let posInArrayPlayer
 
     const playerSelDescrip = `You selected ${playerSelection}`
     const computerSelDescrip = `Computer selected ${computerSelection}`
     const selectionsDescrip = `${playerSelDescrip} - ${computerSelDescrip} - `
 
- 
     let resultDescrip
     let result 
     if (playerSelection === computerSelection){
@@ -49,7 +38,6 @@ let playRound = (computerSelection, playerSelection) => {
 
     const fullDescrip = `${selectionsDescrip}${resultDescrip}`
     const lastMatchDetails = document.querySelector(".last-match-details")
-    console.log(lastMatchDetails)
     lastMatchDetails.textContent=fullDescrip
     return result
 }
@@ -59,34 +47,32 @@ const buttons = document.querySelectorAll("button")
 buttons.forEach((btn) => btn.addEventListener('click', function() {
     const playerSelection = this.textContent.toLowerCase()
     const computerSelection = getComputerSelection()
-    playRound(computerSelection, playerSelection)
+    let roundResult = playRound(computerSelection, playerSelection)
+    if (roundResult === "win"){
+        playerScore++ 
+    }
+    else if (roundResult === "lose"){
+        computerScore++
+    }
+    playerScoreDisp.textContent = `Your Score: ${playerScore}` 
+    computerScoreDisp.textContent = `Computer Score: ${computerScore}`
 }))
 
 
-// let game = () => {
-//     console.log("This is rock paper scissors, 5 rounds will be played")
-//     let computerScore = 0
-//     let playerScore = 0
-//     for (let i=0; i<5; i++){
-//         console.log(`Round ${i}`)
-//         console.log(`Your score: Computer score is ${playerScore}:${computerScore}`)
-//         let roundResult = playRound(getComputerSelection(), getPlayerSelection())
-//         if (roundResult === "win"){
-//             playerScore++ 
-//         }
-//         else if (roundResult === "lose"){
-//             computerScore++
-//         }
-//     }
-    
-//     console.log(`Final Score is ${playerScore}:${computerScore}`)
-//     if (playerScore>computerScore){
-//         console.log("You Win!")
-//     }
-//     else if(playerScore<computerScore){
-//         console.log("You Lose!")
-//     }
-//     else{
-//         console.log("Draw!")
-//     }
+
+
+
+
+
+
+
+
+// if (playerScore>computerScore){
+//     console.log("You Win!")
+// }
+// else if(playerScore<computerScore){
+//     console.log("You Lose!")
+// }
+// else{
+//     console.log("Draw!")
 // }
